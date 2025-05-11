0c82a6c6a0c013232111865ee8af2f50577f37b1
Badro39 <badrdjab39@gmail.com>
2025-05-11 12:21:02 +0100

    index.js : remove csurf configuration

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // libs
-import express from 'express';
 import cors from 'cors';
-import {config} from 'dotenv';
-import helmet from 'helmet';
+import { config } from 'dotenv';
+import express from 'express';
 import rateLimit from 'express-rate-limit';
-import csurf from 'csurf';
+import helmet from 'helmet';
 
 // middleware
 import errorMiddleware from './middleware/error.js';
-import sanitizeMiddleware from "./middleware/sanitize.js"
+import sanitizeMiddleware from './middleware/sanitize.js';
 
 // db connection
 import connectDB from './config/db.js';
 
 // routes
-import donationRoute from "./api/donation.js";
-import webhookRoute from "./api/webhook.js";
-import extraRoute from "./api/extra.js"
+import donationRoute from './api/donation.js';
+import extraRoute from './api/extra.js';
+import webhookRoute from './api/webhook.js';
 
 config();
-connectDB()
+connectDB();
 
 // constants
 const app = express();
@@ -29,35 +28,25 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again later.',
 });
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST'],
-    credentials: true,
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST'],
+  credentials: true,
 };
-
-const csrfProtection = csurf({ cookie: true });
-const Csurf = (req, res, next) => {
-  if (['POST', 'PUT', 'DELETE'].includes(req.method)) {
-    csrfProtection(req, res, next);
-  } else {
-    next();
-  }
-}
 
 // middlewares
 app.use(cors(corsOptions));
 app.use(helmet());
 app.use(limiter);
-//app.use(Csurf)
 
-app.use("/webhook", webhookRoute)
+app.use('/webhook', webhookRoute);
 app.use(express.json());
-app.use(sanitizeMiddleware)
-app.use("/", donationRoute);
-app.use("/extra", extraRoute)
+app.use(sanitizeMiddleware);
+app.use('/', donationRoute);
+app.use('/extra', extraRoute);
 
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log("Server is running on port " + PORT);
-})
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
